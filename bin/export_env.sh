08bd8838941ee72d80402c647df56dd955a0522c
feng.zhang <cst.feng@gmail.com>
2014-08-25 18:05:24 +0900

    update hubot

---
+++ bin/export_env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+heroku config:add HEROKU_URL="http://hubot-dg-test.herokuapp.com"
+heroku config:add HUBOT_GITHUB_ORG="innovationcenter"
+heroku config:add HUBOT_GITHUB_TOKEN="6db8c122922db85fe2978e0715c49ad78678d408"
+heroku config:add HUBOT_SLACK_BOTNAME="Test-Slack-Arthur"
+heroku config:add HUBOT_SLACK_TEAM="DeployGate"
+heroku config:add HUBOT_SLACK_TOKEN="xoxp-2446364950-2445644913-2577809626-bf2e3a"
